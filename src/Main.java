81b5c41bbe4329c5809e4817f86f02befd09db43
cmj-dev <89201424+cmj-dev@users.noreply.github.com>
2022-09-26 20:55:21 +0900

    add if statement to filter user's wrong input. User should put number between 2 and 9

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Please enter number to calculate gugudan! : ");
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int n;
+        do{
+            System.out.print("Please enter number to calculate gugudan! : ");
+            n = scanner.nextInt();
+            if (n < 2 || n > 9) {
+                System.out.println("please enter natural number between 2 and 9");
+            }
+        }
+        while(n < 2 || n > 9);
+
         for(int i = 1; i < 10; i++){
             System.out.printf("%d * %d = %d%n",n, i, n * i );
         }
