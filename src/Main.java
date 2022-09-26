422261b34ebdf10d3b96d246f9b70c3c81398d8e
cmj-dev <89201424+cmj-dev@users.noreply.github.com>
2022-09-26 20:44:19 +0900

    add simple gugudan task when user enter 'n' print 'n'th gugudan

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Please enter number to calculate gugudan! : ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i = 1; i < 10; i++){
+            System.out.printf("%d * %d = %d%n",n, i, n * i );
+        }
     }
 }
